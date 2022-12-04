4662fd184871fd6494ad8640f95ab29fecceffa1
red <red@kalab.sk>
2022-12-04 20:22:28 +0100

    pretty fast day 4

--- src/a04.rs
+++ src/a04.rs
@@ -1,4 +1,31 @@
 pub fn run(inp: &str) -> i64 {
+    inp.as_bytes()
+        .split(|&b| b == b'\n')
+        .filter(|s| !s.is_empty())
+        .filter(|line| {
+            let mut nums: [u32; 4] = [0, 0, 0, 0];
+            let mut ind = 0;
+
+            unsafe {
+                line.iter().for_each(|c| {
+                    if *c < b'0' {
+                        ind += 1;
+                    } else {
+                        *nums.get_unchecked_mut(ind) = nums.get_unchecked(ind) * 10 + *c as u32;
+                    }
+                });
+            }
+
+            let aa = nums[0];
+            let ab = nums[1];
+            let ba = nums[2];
+            let bb = nums[3];
+            (aa <= ba && ba <= bb && bb <= ab) || (ba <= aa && aa <= ab && ab <= bb)
+        })
+        .count() as i64
+}
+
+pub fn run_slow(inp: &str) -> i64 {
     let mut res = 0;
 
     for line in inp.lines() {
